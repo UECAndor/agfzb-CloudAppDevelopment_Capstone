f03cfc9a900e66aa069a14b807c93448d9438de6
UECAndor <andreaslieble@gmail.com>
2022-10-28 07:46:18 -0400

    LAB3.3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, DealerReview, CarModel, CarMake
+from .restapis import get_dealers_from_cf,get_dealer_reviews_from_cf,post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -33,7 +33,7 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 def login_request(request):
     context = {}
-    url = "https://08663624.us-south.apigw.appdomain.cloud/api/dealership"
+    url = "https://eu-de.functions.appdomain.cloud/api/v1/web/IBM-Lab_CLI/dealership-package/get-dealership"
     dealerships = get_dealers_from_cf(url)
     # Concat all dealer's short name
     context["dealership_list"]=dealerships
@@ -57,7 +57,7 @@ def login_request(request):
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     context = {}
-    url = "https://08663624.us-south.apigw.appdomain.cloud/api/dealership"
+    url = "https://eu-de.functions.appdomain.cloud/api/v1/web/IBM-Lab_CLI/dealership-package/get-dealership"
     dealerships = get_dealers_from_cf(url)
     # Concat all dealer's short name
     context["dealership_list"]=dealerships
@@ -103,16 +103,71 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
+        context={}
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/IBM-Lab_CLI/dealership-package/get-dealership"
+        apikey="Uc1J05vYEQcFigh7CXCT9-HhgJtYnk-akIFxchmqLNeC"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        context["dealership_list"]=dealerships
+        # Return a list of dealer short name
         return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
-
+def get_dealer_details(request, dealer_id):
+    context={}
+    url = "https://eu-de.functions.appdomain.cloud/api/v1/web/IBM-Lab_CLI/dealership-package/get-review"
+    apikey="Uc1J05vYEQcFigh7CXCT9-HhgJtYnk-akIFxchmqLNeC"
+    #print(dealer_id)
+    # Get dealers from the URL
+    dealer_details = get_dealer_reviews_from_cf(url,dealer_id)
+    context["dealer_id"]=dealer_id
+    context["reviews"]=dealer_details
+    return render(request, 'djangoapp/dealer_details.html', context)
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    context = {}
+    # If it is a GET request, just render the add_review page
+    if request.method == 'GET':
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/IBM-Lab_CLI/dealership-package/get-dealership"
+        # Get dealers from the URL
+        context = {
+            "dealer_id": dealer_id,
+            "dealer_name": get_dealers_from_cf(url)[dealer_id-1].full_name,
+            "cars": CarModel.objects.all()
+        }
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == 'POST':
+        if (request.user.is_authenticated):
+            review = dict()
+            review["id"]=0
+            review["name"]=request.POST["name"]
+            review["dealership"]=dealer_id
+            review["review"]=request.POST["content"]
+            if ("purchasecheck" in request.POST):
+                review["purchase"]=True
+            else:
+                review["purchase"]=False
+            print(request.POST["car"])
+            if review["purchase"] == True:
+                car_parts=request.POST["car"].split("|")
+                review["purchase_date"]=request.POST["purchase_date"] 
+                review["car_make"]=car_parts[0]
+                review["car_model"]=car_parts[1]
+                review["car_year"]=car_parts[2]
 
+            else:
+                review["purchase_date"]=None
+                review["car_make"]=None
+                review["car_model"]=None
+                review["car_year"]=None
+            json_result = post_request("https://eu-de.functions.appdomain.cloud/api/v1/web/IBM-Lab_CLI/dealership-package/post-review", review, dealerId=dealer_id)
+            print(json_result)
+            if "error" in json_result:
+                context["message"] = "ERROR: Review was not submitted."
+            else:
+                context["message"] = "Review was submited"
+        return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
